99f5c64d904c5c7b9851d13893c423d2fec7bd80
mallimuondu <mallimuondu@gmail.com>
2020-10-07 21:40:17 +0300

    remove try folder

--- try/hom1.py
+++
@@ -1,5 +0,0 @@
-total = [('soda',60),('cake',40)]
-autput = 0
-for store in total:
-    autput += store[1]
-print(autput)
--- try/try1.py
+++
@@ -1,5 +0,0 @@
-a = [5,4,4,1]
-ading = sum(a)
-print(ading)
-#add = [hello+hello for c in a for hello in c]
-#print(add)
